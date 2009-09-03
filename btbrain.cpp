d6f04b294d747049c6a3df9fa5cc2604ff077e4a
Dan Leinir Turthra Jensen <admin@leinir.dk>
2009-09-03 13:46:26 +0200

    Adapt enum naming to recommendations

--- btbrain.cpp
+++ btbrain.cpp
@@ -9,31 +9,31 @@ btBrain::btBrain(QObject *parent)
     btNodeType *node = new btNodeType();
     node->setName("Sequence");
     node->setDescription("A sequence of behaviors, launched in order (fails if one fails)");
-    node->setCategory(btNodeType::Composite);
+    node->setCategory(btNodeType::CompositeCategory);
     nodeTypes.append(node);
 
     node = new btNodeType();
     node->setName("Selector");
     node->setDescription("A collection of behaviors which are launched in order, until one succeeds (only fails if all fails)");
-    node->setCategory(btNodeType::Composite);
+    node->setCategory(btNodeType::CompositeCategory);
     nodeTypes.append(node);
 
     node = new btNodeType();
     node->setName("Parallel");
     node->setDescription("A collection of behaviors which are launched at the same time");
-    node->setCategory(btNodeType::Composite);
+    node->setCategory(btNodeType::CompositeCategory);
     nodeTypes.append(node);
 
     node = new btNodeType();
     node->setName("Random");
     node->setDescription("A collection of behaviors from which is selected one to be launched");
-    node->setCategory(btNodeType::Composite);
+    node->setCategory(btNodeType::CompositeCategory);
     nodeTypes.append(node);
 
     node = new btNodeType();
     node->setName("InBehavior");
     node->setDescription("Check whether you are in a specified behavior");
-    node->setCategory(btNodeType::Condition);
+    node->setCategory(btNodeType::ConditionCategory);
     nodeTypes.append(node);
 }
 
@@ -62,7 +62,7 @@ btTreeModel *btBrain::newBehaviorTree()
     btNodeType *newType = new btNodeType(this);
     newType->setName(newTree->name());
     newType->setDescription(tr("A reference to the behavior tree named %1").arg(newTree->name()));
-    newType->setCategory(btNodeType::Reference);
+    newType->setCategory(btNodeType::ReferenceCategory);
     
     // Finally inform those around us of this wonderful occurrence 
     emit behaviorTreeAdded(newTree);
