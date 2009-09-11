e3c808dbbe56e90a160c0bc5ba06984dd800b233
Morten Justesen <morten.justesen@gmail.com>
2009-09-11 13:39:14 +0200

    minor fix missing return

--- src/gluon-bt-lib/btnodetypefactory.cpp
+++ src/gluon-bt-lib/btnodetypefactory.cpp
@@ -4,15 +4,16 @@ btNodeType* btNodeTypeFactory::newObject(QString classname) {
     btNodeType* node = 0;
     if(classname == "Selector"){
         node = new btSelectorNode();
-        return node;
+        node->setName("Selector");
     }
+    return node;
 }
 
 btNodeTypeFactory* btNodeTypeFactory::instance() {
     static btNodeTypeFactory* singleton = 0;
     if(singleton == 0){
         singleton = new btNodeTypeFactory();
-        //singleton->setParent(qApp); fix
+        //singleton->setParent(qApp);
     }
     return singleton;
 }
