e515b0b4a6e12e59c24d05ae446c877195a1f515
unknown <admin@.(none)>
2009-09-11 10:51:20 +0200

    initial btlib

---
+++ src/gluon-bt-lib/btnodetype.cpp
@@ -0,0 +1,31 @@
+#include "btnodetype.h"
+
+btNodeType::btNodeType(QObject * parent)
+{
+}
+
+btNodeType::~btNodeType()
+{
+}
+
+void btNodeType::setName(QString name) { m_name = name; }
+QString btNodeType::name() const { return m_name; }
+
+void btNodeType::setDescription(QString description) { m_description = description; }
+QString btNodeType::description() const { return m_description; }
+
+void btNodeType::setNodeType(nodeType type){m_type = type;}
+btNodeType::nodeType btNodeType::type() const
+{
+    if(m_type)
+        return m_type;
+    else
+        return UnusableNodeType;
+}
+
+bool btNodeType::run()
+{
+	return false;
+}
+
+#include "btnodetype.moc"
---
+++ src/gluon-bt-lib/btnodetype.h
@@ -0,0 +1,43 @@
+#ifndef BTNODETYPE_H
+#define BTNODETYPE_H
+
+#include <QObject>
+
+class btNodeType : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(QString name READ name WRITE setName)
+    Q_PROPERTY(QString description READ description WRITE setDescription)
+    Q_PROPERTY(nodeType type READ type WRITE setNodeType)
+	Q_ENUMS(nodeType)
+	
+public:
+	
+	enum nodeType
+	{
+		UnusableNodeType = -1,
+		ActionNodeType,
+		CompositeNodeType,
+		ConditionNodeType,
+		ReferenceNodeType,
+        DecoratorNodeType
+	};
+	
+    btNodeType(QObject* parent = 0);
+    ~btNodeType();
+
+    void setName(QString name);
+	QString name() const;
+    void setDescription(QString description);
+    QString description() const;
+    nodeType type() const;
+	void setNodeType(nodeType type);
+	virtual bool run();
+	
+protected:
+    QString m_name;
+    QString m_description;
+    nodeType m_type;
+};
+
+#endif // BTNODETYPE_H
