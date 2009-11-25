48be9f669fc52527e393ecd66cf91cc15b5c4d5b
Kim Jung Nissen <kimjung@h74.k2.cs.aau.dk>
2009-11-25 09:23:59 +0100

    missing files :D

---
+++ src/gluon-bt-editor/btchildlistwidget.cpp
@@ -0,0 +1,51 @@
+#include "btchildlistwidget.h"
+
+#include <QLabel>
+#include <QDoubleSpinBox>
+#include <QVariant>
+
+#include "btnode.h"
+#include "bteditornodetype.h"
+
+btChildListWidget::btChildListWidget(QWidget * parent) : QWidget(parent)
+{
+    childLayout = new QVBoxLayout(this);
+}
+
+btChildListWidget::~btChildListWidget()
+{
+}
+
+void btChildListWidget::setChildProbabilites(QObject* object)
+{
+    btEditorNodeType * nodeType = qobject_cast<btEditorNodeType*>(object);
+    btNode* node = qobject_cast<btNode*>(nodeType->parentNode());
+    
+    for (int i = 0; i < node->childCount(); i++) 
+    {        
+        QLabel * nodeName = new QLabel(this);
+        nodeName->setText(node->child(i)->name());
+        
+        QDoubleSpinBox * prob = new QDoubleSpinBox(this);
+        prob->setRange(0.0, 1.0);
+        prob->setSingleStep(0.01);
+        
+        if(!node->child(i)->type()->property("probability").isValid())
+        {
+            node->child(i)->type()->setProperty("probability", 0.0);
+        }
+        
+        prob->setValue(node->child(i)->type()->property("probability").toDouble());
+        
+        btEditorNodeType* editorNodeType = qobject_cast<btEditorNodeType*>(node->child(i)->type());
+        
+        connect(prob, SIGNAL(valueChanged(double)), editorNodeType, SLOT(changeProbability(double)));
+        
+        childLayout->addWidget(nodeName);
+        childLayout->addWidget(prob);
+
+    }
+    this->setLayout(childLayout);
+}
+
+#include "btchildlistwidget.moc"
