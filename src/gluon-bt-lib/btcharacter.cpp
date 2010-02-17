4387e70a7bae0782c5ce0c61aac1878837cdbf04
Kim Jung Nissen <jungnissen@h74.k2.cs.aau.dk>
2010-02-17 09:13:35 +0100

    removed unecessary code

--- src/gluon-bt-lib/btcharacter.cpp
+++ src/gluon-bt-lib/btcharacter.cpp
@@ -22,33 +22,23 @@ void btCharacter::setBehaviorTree(btNode* behaviorTree)
 void btCharacter::think()
 {
 	btNode* currentNode = m_currentNode.top();
-	//currentNode->setCurrentChildIndex(m_currentchild.top());
-	qDebug() << m_currentNode.count();
+	currentNode->setCurrentChildIndex(m_currentchild.top());
 	
 	m_nodeStatus = currentNode->run(this);
 #warning remember to set the child status again :)
 	switch (m_nodeStatus)
 	{
 		case btNode::RunningChild:
-			//getting child for execusing in next tick.
-			//m_currentNode = m_currentNode->currentChild();
 			m_currentNode.push(currentNode->currentChild());
 			m_currentChild.push(currentNode->currentChildIndex());
 			break;
 		case btNode::Failed:
 		case btNode::Succeeded:
-			//resetting current child
-			//currentNode->setCurrentChildIndex(0);
-			//currentNode->setCurrentChildStatus(btNode::None);
-			//aquiring the parent
-			//m_currentNode = m_currentNode->parentNode();
 			if(m_currentNode.top() != m_behaviortree)
 			{
 				m_currentNode.pop();
 				m_currentChildIndex = m_currentChild.pop();
 			}
-			//setting the childstatus of the parent node
-			//currentNode->setCurrentChildStatus(m_nodeStatus);
 			break;
 	}
 }
